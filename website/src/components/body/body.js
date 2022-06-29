a9faeb149681ff81cbbccb2c468a7c4e002361b1
tasniim <tasniim@MacBook-Pro.local>
2022-06-29 22:50:17 +0100

    created body div for logo and texh-used

--- website/src/components/body/body.js
+++ website/src/components/body/body.js
@@ -1,9 +1,53 @@
+import style from "./body.module.css"
+import{useState, useEffect} from "react"
+import profile from "../../logo1.svg"
+import {techStack} from "../../techstake.js"
+
 function Body(){
-    return(
-        <h1>
-            this is a body
-        </h1>
-    )
+
+    const [width, setWidth]=useState("");
+    
+    useEffect(()=>{
+        function dynamicWidth(){
+            setWidth(window.innerWidth);
+        }
+
+        window.addEventListener('resize', dynamicWidth)
+        dynamicWidth()
+    },[width]);
+
+    return (
+      <div className={style.mainBodyContainer} style={{ width: `${width}px` }}>
+        <div className={style.mainContainer}>
+          <img src={profile} alt="logo"></img>
+
+          <div className={style.inputField}>
+            <h1>input field</h1>
+          </div>
+
+          <div className={style.techStackContainer}>
+            <div className={style.techStackHeader}>
+              <p>Current Tech-Stack</p>
+            </div>
+            <div className={style.techStackImage}>
+            {techStack.map((input,index)=>{
+
+                return (
+                  <div className={style.techStackcicle} key={index}>
+                    <a href={input.address} target="_blank" rel="noreferrer">
+                      <img src={input.img} alt={input.alt}></img>
+                    </a>
+                  </div>
+                );
+                
+                })  
+
+            }
+            </div>
+          </div>
+        </div>
+      </div>
+    );
 }
 
 export default Body;
---
+++ website/src/techstake.js
@@ -0,0 +1,93 @@
+export const techStack = [
+  {
+    img: "https://play-lh.googleusercontent.com/85WnuKkqDY4gf6tndeL4_Ng5vgRk7PTfmpI4vHMIosyq6XQ7ZGDXNtYG2s0b09kJMw",
+    alt: "html",
+    address:
+      "https://developer.mozilla.org/en-US/docs/Learn/Getting_started_with_the_web/HTML_basics",
+  },
+  {
+    img: "https://camo.githubusercontent.com/119b29ca4b9d31cf3969a94eb57fcfbbea0879b493c09c89dc6d4b7fb9e0dc37/68747470733a2f2f63646e2e776f726c64766563746f726c6f676f2e636f6d2f6c6f676f732f6373732d332e737667",
+    alt: "css",
+    address: "https://developer.mozilla.org/en-US/docs/Web/CSS",
+  },
+  {
+    img: "https://www.brcline.com/wp-content/uploads/2020/06/javascript-logo.png",
+    alt: "javascript",
+    address: "https://developer.mozilla.org/en-US/docs/Web/JavaScript",
+  },
+  {
+    img: "https://i.pinimg.com/736x/4d/13/d5/4d13d55d6d0b38b7a4e85fcf97ff6279.jpg",
+    alt: "phyton",
+    address: "https://en.wikipedia.org/wiki/Python_(programming_language)",
+  },
+  {
+    img: "https://avatars.githubusercontent.com/u/6412038?s=280&v=4",
+    alt: "react",
+    address: "https://reactjs.org/",
+  },
+  {
+    img: "https://miro.medium.com/max/449/1*xgpVdDtCINFHGNgrEtI8iQ.png",
+    alt: "cypress",
+    address: "https://www.cypress.io/",
+  },
+  {
+    img: "https://iconape.com/wp-content/png_logo_vector/jest-logo.png",
+    alt: "jest",
+    address: "https://jestjs.io/",
+  },
+  {
+    img: "https://avatars.githubusercontent.com/u/2918581?s=280&v=4",
+    alt: "bootstrap",
+    address: "https://getbootstrap.com/",
+  },
+  {
+    img: "https://www.payoda.com/wp-content/uploads/2021/05/1_mbP4Bjrs8Hshx7IgjsUNSg.jpeg",
+    alt: "rest",
+    address: "https://www.ibm.com/cloud/learn/rest-apis",
+  },
+  {
+    img: "https://w7.pngwing.com/pngs/545/451/png-transparent-node-js-express-js-javascript-solution-stack-web-application-others-angle-text-rectangle.png",
+    alt: "express",
+    address: "https://expressjs.com/en/guide/routing.html",
+  },
+  {
+    img: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRrRwTkfJDlrB-NTf4r7Ez42QnKUPVBtewst4bW9Mu2GK5cRYwMO3VkbITLbG9TMaQJeQI&usqp=CAU",
+    alt: "node",
+    address: "https://nodejs.org/en/",
+  },
+  {
+    img: "https://media.trustradius.com/product-logos/QY/QH/HFBO8T3NTXUM-180x180.PNG",
+    alt: "mysql",
+    address: "https://www.mysql.com/",
+  },
+  {
+    img: "https://res.cloudinary.com/hevo/image/upload/f_auto,q_auto/v1626694700/hevo-blog/MongoDB-sm-logo-500x400-1-1.gif",
+    alt: "mongoDB",
+    address: "https://www.mongodb.com/brand-resources",
+  },
+  {
+    img: "https://media-exp2.licdn.com/dms/image/C560BAQGvV_5x3UBMJA/company-logo_200_200/0/1571158216754?e=2147483647&v=beta&t=1R7lGc6aLMqZ_I_wFNyIRstcCnnbiDe6oY4ESXQlP48",
+    alt: "figma",
+    address: "https://www.figma.com/using-the-figma-brand/",
+  },
+  {
+    img: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQh1PLOOvsnd1QmDS5ou7lUAnJ3WYKg9hpuhLrnN_Y1KusfKBqZmj1LE1z_1HpEN_IJ56g&usqp=CAU",
+    alt: "xdesign",
+    address: "https://www.adobe.com/uk/products/xd.html",
+  },
+  {
+    img: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQbBCowzQIMd3kFv3ZqKElcPJYYbmV9mYDx2qf04515sWy4l7YdrWNWzVqPGiuLC3xGA2I&usqp=CAU",
+    alt: "inkscape",
+    address: "https://inkscape.org/",
+  },
+  {
+    img: "https://res.cloudinary.com/crunchbase-production/image/upload/c_lpad,h_256,w_256,f_auto,q_auto:eco,dpr_1/nb3m4vdppw5kwqfnptk9",
+    alt: "webflow",
+    address: "https://webflow.com/",
+  },
+  {
+    img: "https://cdn-icons-png.flaticon.com/512/5968/5968762.png",
+    alt: "wix",
+    address: "https://www.wix.com/",
+  },
+];
